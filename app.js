26e6db27b936e0ffb3d4564fdc3d6dbc684ec5f8
vaayou <65039033+vaayou@users.noreply.github.com>
2022-07-24 11:31:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ app.post("/failure", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("server is running on port 3000");
+app.listen(process.env.PORT, function () {
+  console.log("server is running on port PORT");
 });
